b18deec9a3bd2f35b42eb1bbbf8c7070c31a486f
Prasun Suwal <prasun.suwal@gmail.com>
2024-08-09 23:32:34 +0545

    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,104 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import About from './components/About';
+import Alert from './components/Alert';
+import About from './components/About';
+import React, {useState} from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-let name ="Prasun";
+
+// let name ="Prasun";
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+      
+    }, 1500);
+  }
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled","success");
+      document.title = 'TextUtils -DarkMode';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled","success");
+      document.title = 'TextUtils -LightMode';
+    }
+  }
+  //FOR ROUTING
   return (
-    // <div className="App">
-    //   <Greet />
-    // </div>
-  <>
-    {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
-    <Navbar title="TextUtils"/>
-    <div className="container my-3">
-    <TextForm heading="Enter the text to analyze"/>
-    {/* <About/> */}
-    </div>
+    <>
+    <BrowserRouter>
+      <Navbar
+        title="TextUtils2"
+        aboutText="TextAbouts"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      <Alert alert={alert} mode={mode} />
+      <div className="container my-4" mode={mode}>
+        <Routes>
+          <Route exact path="/about" element={<About mode={mode} />}>
+          </Route>
+          <Route
+            exact path="/"
+            element={
+              <TextForm
+                showAlert={showAlert}
+                heading="Try TextUtils- Word Counter, Character Counter, Remove Extraspaces"
+                mode={mode}
+              />
+            }
+          ></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   </>
-  );
+);
 }
 
 export default App;
+
+//FOR ROUTING
+// return (
+//   <>
+//     <BrowserRouter>
+//       <Navbar
+//         title="TextUtils2"
+//         aboutText="TextAbouts"
+//         mode={mode}
+//         toggleMode={toggleMode}
+//       />
+//       <Alert alert={alert} />
+//       <div className="container my-4" mode={mode}>
+//         <Routes>
+//           <Route exact path="/about" element={<About />}></Route>
+//           <Route
+//             exact path="/"
+//             element={
+//               <Textbox
+//                 showAlert={showAlert}
+//                 heading="Enter Text to analyze "
+//                 mode={mode}
+//               />
+//             }
+//           ></Route>
+//         </Routes>
+//       </div>
+//     </BrowserRouter>
+//   </>
+// );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,59 +1,75 @@
-import React, {useState} from 'react'
+ import React, {useState} from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = ()=>{
         // console.log("Uppercase was clicked" + text);
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted to UpperCase!", "success");
     }
     const handleLoClick = ()=>{
         let newText = text.toLocaleLowerCase();
         setText(newText);
+        props.showAlert("Converted to LowerCase!", "success");
+
     }
     const handleClearClick = ()=>{
         let newText = "";
         setText(newText);
+        props.showAlert("Text has been cleared","success");
     }
     const handleCopy = ()=>{
-        var text = document.getElementById("myBox");
-        text.select();
-        navigator.clipboard.writeText(text.value);
+        
+        navigator.clipboard.writeText(text);
+        
+        props.showAlert("Text has been copied!!!", "success");
     }
     const handleExtraSpace = ()=>{
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "));
+        props.showAlert("Extra space has been removed!!", "success");
     }
     const speak = () => {
         let msg = new SpeechSynthesisUtterance();
         msg.text = text;
         window.speechSynthesis.speak(msg);
+        
       }
 
     const handleOnChange = (event)=>{
         // console.log("On change");
         setText(event.target.value)
     }
+    // const wordCount = (text)=>{
+    //     let regex = /\s+\S+/;
+    //     let numOfWords = text.split(regex);
+    //     return numOfWords.length;
+    //   }
     const [text, setText] = useState('');
     return (
         <>
-        <div className='container'>
-            <h1>{props.heading}</h1>
+        <div className='container' style={{color: props.mode === 'dark'?'white':'#042743'}}>
+            <h1 className='mb-3'>{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'?'#13466e':'white', color: props.mode === 'dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert To UpperCase</button>
-            <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert To LowerCase</button>
-            <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
-            <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
-            <button className="btn btn-primary mx-1" onClick={handleExtraSpace}>Remove Extra Spaces</button>
-            <button className="btn btn-warning mx-2 my-2" type="submit" onClick={speak} >Speak</button>
+            <button disabled={text.length === 0} className="btn btn-primary mx-1" onClick={handleUpClick}>Convert To UpperCase</button>
+            <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert To LowerCase</button>
+            <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+            <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+            <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpace}>Remove Extra Spaces</button>
+            <button disabled={text.length === 0} className="btn btn-warning mx-2 my-2" type="submit" onClick={speak} >Speak</button>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color: props.mode === 'dark'?'white':'#042743'}}>
             <h2>Your Text Summary</h2>
-            <p>{text.split(" ").length} words and {text.length} characters</p>
-            <p>Takes {0.008 * text.split(" ").length} minutes to read</p>
+            {/* <p>{text.split(" ").length} words and {text.length} characters</p> */}
+            {/* <p>{text===""? 0 : wordCount(text)} words and {text.length} characters</p>
+            
+            <p>Takes {0.008 * text.split(" ").filter((element)=>{return element.lenght!==0}).length} minutes to read</p> */}
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+            <p>{0.008 *  text.split(/\s+/).filter((element)=>{return element.length!==0}).length} Minutes read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Nothing to preview"}</p>
         </div>
         </>
     )
